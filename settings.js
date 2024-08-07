c15c809c1ccaa72695bf2ff2493535c08ed8d518
AhmadAmin02 <166785197+AhmadAmin02@users.noreply.github.com>
2024-08-07 19:27:54 +0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['!','.',',','🐤','🗿']
+
+global.owner = ['6289529068349']
+global.botname = 'gatau gw'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Yahaha"
+global.sticker2 = "🌜"
